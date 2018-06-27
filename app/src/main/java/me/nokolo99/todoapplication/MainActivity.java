5d640c4f0eacc54a1b7f5b2ab10b97f4ba082bfa
Nneka Okolo <nokolo@fb.com>
2018-06-27 16:31:10 -0700

    Edit Functionality Added

--- app/src/main/java/me/nokolo99/todoapplication/MainActivity.java
+++ app/src/main/java/me/nokolo99/todoapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package me.nokolo99.todoapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -19,6 +20,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    // a numeric code to identify the edit activity
+    public static final int EDIT_REQUEST_CODE = 20;
+    // keys used for passing data between activities
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
+
+    // rest of class
 
     // declaring stateful object here; these will be null before onCreate is called
     ArrayList<String> items;
@@ -47,6 +55,26 @@ public class MainActivity extends AppCompatActivity {
         // setup the listener on creation
         setupListViewListener();
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // EDIT_REQUEST_CODE defined with constants
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            // extract updated item value from result extras
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            // get the position of the item which was edited
+            int position = data.getExtras().getInt(ITEM_POSITION, 0);
+            // update the model with the new item text at the edited position
+            items.set(position, updatedItem);
+            // notify the adapter the model changed
+            itemsAdapter.notifyDataSetChanged();
+            // Store the updated items back to disk
+            writeItems();
+            // notify the user the operation completed OK
+            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onAddItem(View v) {
@@ -80,6 +108,20 @@ public class MainActivity extends AppCompatActivity {
 
                 // return true to tell the framework that the long click was consumed
                 return true;
+            }
+        });
+
+        // set the ListView's regular click listener
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // first parameter is the context, second is the class of the activity to launch
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                // put "extras" into the bundle for access in the edit activity
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+                // brings up the edit activity with the expectation of a result
+                startActivityForResult(i, EDIT_REQUEST_CODE);
             }
         });
     }
